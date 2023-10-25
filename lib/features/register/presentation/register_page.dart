1f00a7d7bebacaa276deb3c1a8cd866d46ff2eb4
Authority10240 <48833225+Authority10240@users.noreply.github.com>
2023-10-25 10:15:53 +0200

    registration feature work in progress

--- lib/features/register/presentation/register_page.dart
+++ lib/features/register/presentation/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_ui_components/flutter_ui_components.dart';
 import 'package:pickme/base_classes/base_page.dart';
 import 'package:pickme/core/locator/locator.dart';
 import 'package:pickme/features/register/domain/entities/user/user.dart';
@@ -27,48 +28,77 @@ class _RegisterPageState extends BasePageState<RegisterPage,RegisterBloc> {
   @override
   Widget buildView(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
-      ),
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
       child: Form(
-        child: Column(
+        child: Stack(
           children:[
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20 , top: 10, bottom: 10),
-            child: TextFormField(decoration: InputDecoration(hintText: getLocalization().firstName,))
-            ,
-          ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20 , top: 10, bottom: 10),
-              child: TextFormField(decoration: InputDecoration(hintText: getLocalization().surname))
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20 , top: 10, bottom: 10),
-              child: TextFormField(decoration: InputDecoration(hintText: getLocalization().phoneNumber))
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20 , top: 10, bottom: 10),
-              child: TextFormField(decoration: InputDecoration(hintText: getLocalization().emailAddress))
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20 , top: 10, bottom: 10),
-              child:TextFormField(decoration: InputDecoration(hintText: getLocalization().idNumber))
-            ),
-            const Spacer(),
+            Positioned(top: 0,child:
+            Container(
+              color: Colors.grey,
+              height: MediaQuery.sizeOf(context).height *(1/3) ,
+              width: MediaQuery.sizeOf(context).width,)),
 
-            ElevatedButton(onPressed: (){
-            /*  getBloc().add(ContinueClickedEvent(user: User(
-                email: "fmathebula@dvtsoftware.com",
-                workPermitNumber: "",
-                surname: "DVT",
-                subscriptionType: "",
-                profileType: "",
-                passportNumber: "",
-                mobile: "436547673575375",
-                idNumber: "546764867674",
-                firstName: "Freedom"
-              )));*/
-            }, child:Text(getLocalization().ccontinue) )
+            Positioned(bottom: 0,
+              child: Container(height: MediaQuery.sizeOf(context).height * (2/3) ,
+                width: MediaQuery.sizeOf(context).width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(32), topLeft: Radius.circular(32) ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top:30.0, bottom: 20, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only( top: 10, bottom:  10),
+                        child: AppTextFormField(
+                          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom:  10),
+                          textFieldType: TextFieldType.NAME, labelText: getLocalization().firstName,),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only( top: 10, bottom:  10),
+                        child: AppTextFormField(
+                          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom:  10),
+                          textFieldType: TextFieldType.NAME, labelText: getLocalization().surname,),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom:  10),
+                        child: AppTextFormField(
+                          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom:  10),
+                          textFieldType: TextFieldType.NAME, labelText: getLocalization().phoneNumber,),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only( top: 10, bottom:  10),
+                        child: AppTextFormField(
+                          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom:  10),
+                          textFieldType: TextFieldType.NAME, labelText: getLocalization().emailAddress,),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom:  10),
+                        child: AppTextFormField(
+                          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom:  10),
+                          textFieldType: TextFieldType.NAME, labelText: getLocalization().idNumber,),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+
+
+
+
+            Positioned(
+              bottom: 20,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 20,left: 20,top: 10, bottom: 10),
+                child: PrimaryButton(width: MediaQuery.sizeOf(context).width - 45,onPressed: () async {
+                },
+                    child: Text(getLocalization().ccontinue)),
+              ),
+            ),
         ],
       ),
     ),
