73d42302e6fe794496231376926731ce6ac4fb70
Andrew Sithole <andrewsithole7@gmail.com>
2024-03-20 14:07:59 +0200

    Redirect to Final page (subscription page) if a person has not paid

--- lib/shared/mixins/route_page_mixin.dart
+++ lib/shared/mixins/route_page_mixin.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../navigation/app_route.dart';
 import '../features/otp/domain/entities/profile_entity.dart';
-import '../in_app_purchases/presentation/in_app_purchase_bloc.dart';
 
 mixin RoutePageMixin{
   void routePageReg({required BuildContext context, required ProfileEntity profileEntity, int from = 0}){
@@ -18,14 +13,8 @@ mixin RoutePageMixin{
       context.router.push(const BankDetailsRoute());
     }else if(profileEntity.location!.address == "" ){
       context.router.push(const LocationRoute());
-    }else if(profileEntity.description!.isEmpty){
+    }else if(profileEntity.description!.isEmpty || !profileEntity.subscriptionPaid!){
       context.router.push(const FinalDetailsRoute());
-    }else if(!profileEntity.subscriptionPaid!) {
-      if(Platform.isIOS){
-        BlocProvider.of<InAppPurchasesBloc>(context).add(CreateSubscriptionEvent());
-      }else{
-        context.router.push( PaySomeoneWebViewRoute(from: from));
-      }
     }else{
       context.router.pushAndPopUntil( BottomNavigationBarRoute(), predicate: (Route<dynamic> route) => false);
     }
