2f71ede5bdad72b96b952436b11e0722a58364e8
Serge Mbamba <sm.mbamba@gmail.com>
2024-03-27 14:19:24 +0200

    [hotfix]Add missing type

--- lib/shared/features/upload_file/data/repository_impl/upload_file_repository_impl.dart
+++ lib/shared/features/upload_file/data/repository_impl/upload_file_repository_impl.dart
@@ -2,16 +2,15 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import '../../../../../core/exceptions/data_error.dart';
+import '../../../../../localization/generated/l10n.dart';
 import 'package:pickme/shared/features/upload_file/domain/entities/uploaded_file_entity.dart';
 import 'package:pickme/shared/features/upload_file/domain/repository/upload_file_repository.dart';
 import 'package:pickme/shared/services/remote/api_service/upload_file_service/upload_file_service.dart';
 
-import '../../../../../localization/generated/l10n.dart';
-
 
 @Injectable(as: UploadFileRepository)
 class UploadFileRepositoryImpl extends UploadFileRepository {
-  final localization;
+  final AppLocalizations localization;
   final UploadFileService uploadFileService;
 
   UploadFileRepositoryImpl({required this.uploadFileService, required this.localization});
